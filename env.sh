621aa57dd9347c3918a1741d341c3e2dd9fd6da4
Simon Pasquier <spasquie@redhat.com>
2026-09-21 13:53:25 +0200

    Update install.sh

--- env.sh
+++ env.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
+HELM_COMMAND=${HELM_COMMAND:-install}
 PROJECT=${PROJECT:-garage}
+
+info() {
+	echo " 🔔 $*"
+}
--- install.sh
+++ install.sh
@@ -4,32 +4,40 @@ set -euo pipefail
 
 . env.sh
 
-HELM_COMMAND=${HELM_COMMAND:-install}
-
-echo "Checking presence of $PROJECT project..."
+info "Checking presence of $PROJECT project..."
 if [ -z "$(oc get project "${PROJECT}" --ignore-not-found)" ]; then
-	echo "Creating $PROJECT project..."
-	oc new-project garage
+	info "Creating $PROJECT project..."
+	oc new-project "${PROJECT}"
 fi
 
 GARAGE_UID=$(oc get project "${PROJECT}" -o json |  jq -cr '.metadata.annotations["openshift.io/sa.scc.uid-range"] | split("/")[0] | tonumber')
 
-if [ ! -d scripts/helm ]; then
+if [ ! -d garage/scripts/helm ]; then
 	git submodule init
 	git submodule update
 fi
 
-echo "Installing garage with uid $GARAGE_UID..."
+info "Installing garage with uid $GARAGE_UID..."
 VALUES_FILE="$(mktemp)"
 cat <<EOF > "$VALUES_FILE"
 podSecurityContext: null
+monitoring:
+  metrics:
+    enabled: true
 EOF
+
 helm "$HELM_COMMAND" --namespace "${PROJECT}" garage ./garage/script/helm/garage -f "$VALUES_FILE"
+info "Waiting for pods to be ready..."
 oc wait -n "$PROJECT" --for=condition=Ready pods -l app.kubernetes.io/name=garage --timeout=60s
 
-echo "Creating default layout..."
+info "Current status..."
+oc exec -ti -n "${PROJECT}" -c garage garage-0 -- ./garage status
+
+info "Creating default layout..."
 for NODE in $(oc exec -ti -n "$PROJECT" -c garage garage-0 -- ./garage status  | tail -n 3 | awk '{print $1}'); do
-	echo "Assigning $NODE..."
+	info "Assigning $NODE..."
 	oc exec -ti -n "$PROJECT" -c garage garage-0 -- ./garage layout assign -z dc1 -c 1G "$NODE";
 done
+
+info "Commiting layout changes..."
 oc exec -ti -n "$PROJECT" -c garage garage-0 -- ./garage layout apply --version 1
